fc3716cec52717a9cca6a83e892235d36279b7d1
Win Ko Ko <winkoslb2015@gmail.com>
2025-09-28 10:03:48 +0630

    second commit

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to the docker image app");
+    res.send("Welcome to docker image app");
 });
 
 app.listen(3000,function(){
